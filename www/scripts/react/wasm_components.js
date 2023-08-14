1d171b463d8413b7bc02e640300b970fb080b5b6
Eugeny Goryachev <gorcom2012@gmail.com>
2023-08-14 17:42:46 +0400

    Перенес WASM в модальное окно

--- www/scripts/react/wasm_components.js
+++ www/scripts/react/wasm_components.js
@@ -9,7 +9,7 @@ class Wasm extends React.Component {
   
   
   componentWillMount() {				
-		// This is a polyfill for FireFox and Safari
+/*			// This is a polyfill for FireFox and Safari
             if (!WebAssembly.instantiateStreaming) { 
                 WebAssembly.instantiateStreaming = async (resp, importObject) => {
                     const source = await (await resp).arrayBuffer()
@@ -38,13 +38,74 @@ class Wasm extends React.Component {
              console.log("main.wasm is loaded 👋")
          }).catch(error => {
              console.log("ouch", error)
-         })     
-  }
+         })  
+         
+  */       
+  
+  const go = new Go();
+
+  const runWasm = async () => {
+  const importObject = go.importObject;
+  const wasmModule = await wasmBrowserInstantiate("GUI.wasm", importObject);
+  go.run(wasmModule.instance);
+
+  // Get our exports object, with all of our exported Wasm Properties
+  const exports = wasmModule.instance.exports;
+
+  // Get our memory object from the exports
+  const memory = exports.memory;
+
+  // Create a Uint8Array to give us access to Wasm Memory
+  const wasmByteMemoryArray = new Uint8Array(memory.buffer);
+
+  // Get the pointer (index) to where our graphics buffer is located in wasm linear memory
+  const graphicsBufferPointer = exports.getGraphicsBufferPointer();
+  const graphicsBufferSize = exports.getGraphicsBufferSize();
+
+  const canvasElement = document.querySelector("canvas");
+  const canvasContext = canvasElement.getContext("2d");
+  const canvasImageData = canvasContext.createImageData(canvasElement.width, canvasElement.height);
+  
+  canvasElement.onclick = function() {
+  		exports.eventClick(event.offsetX, event.offsetY);
+	}
+
+  // Clear the canvas
+  canvasContext.clearRect(0, 0, canvasElement.width, canvasElement.height);
+
+  const drawCheckerBoard = () => {
+    const checkerBoardSize = 20;
+
+    // Generate a new checkboard in wasm
+    exports.generateCheckerBoard();
+
+    // Pull out the RGBA values from Wasm memory, the we wrote to in wasm,
+    // starting at the checkerboard pointer (memory array index)
+    const imageDataArray = wasmByteMemoryArray.slice(graphicsBufferPointer, graphicsBufferPointer + graphicsBufferSize);
+    canvasImageData.data.set(imageDataArray);
+
+    canvasContext.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    canvasContext.putImageData(canvasImageData, 0, 0);
+  };
+
+  //drawCheckerBoard();
+  setInterval(() => {
+    drawCheckerBoard();
+  }, 150);
+};
+runWasm();
+//console.log("RUN WASM")
+	}
+  
+  
+    
   
 
   render() {
         return (
-        	<div id="body_wasm"></div>
+        	<div id="body_wasm">
+        		<canvas id="cnvs" width="640" height="480"></canvas>
+        	</div>
         );
 
   }
