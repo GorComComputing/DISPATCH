0f3f82ad2879ae4fdd036cc6e597dd817296fde1
Eugeny Goryachev <gorcom2012@gmail.com>
2023-08-24 18:30:49 +0400

    -//-

--- main.go
+++ main.go
@@ -158,6 +158,11 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	
+	/*if strings.Trim(r.URL.Path, "/") == "save" {
+		save(w, r)
+		return
+	}*/
+	
 		
 	// serve static assets from 'static' dir:
 	h.fileServer.ServeHTTP(w, r)
